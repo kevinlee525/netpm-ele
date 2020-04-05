dfa3c948a1b08e0b6c18712d26b88fd7d4ded985
kevinlee525 <kevinlee_168@163.com>
2020-04-05 16:38:44 +0800

    增加了出口带宽bar显示

--- src/views/dashboard/DashBoard.vue
+++ src/views/dashboard/DashBoard.vue
@@ -3,7 +3,7 @@
         <div class="showTime"></div>
         <div class="left">
             <div class="panel bar">
-                <h2>柱形图-就业行情</h2>
+                <h2>DC01-BGP-PD</h2>
                 <div class="chart" ref="leftBar"></div>
                 <div class="panel-footer"></div>
             </div>
@@ -23,8 +23,8 @@
                 </div>
                 <div class="no-bd">
                     <ul>
-                        <li>前端需求人数</li>
-                        <li>市场供应人数</li>
+                        <li>公网总带宽</li>
+                        <li>专线总带宽</li>
                     </ul>
                 </div>
             </div>
@@ -54,21 +54,29 @@
 
 <script>
     var t = null;
+
     function time() {
         clearTimeout(t);//清除定时器
         let dt = new Date();
         var y = dt.getFullYear();
-        var mt = dt.getMonth() + 1;
-        var day = dt.getDate();
-        var h = dt.getHours();//获取时
-        var m = dt.getMinutes();//获取分
-        var s = dt.getSeconds();//获取秒
-        document.querySelector(".showTime").innerHTML = '当前时间：' + y + "年" + mt + "月" + day + "日" + "-" + h + ":" + m + ":" + s;
+        var mt = dt.getMonth() + 1 + '';
+        var day = dt.getDate() + '';
+        var h = dt.getHours() + '';//获取时
+        var m = dt.getMinutes() + '';//获取分
+        var s = dt.getSeconds() + '';//获取秒
+        document.querySelector(".showTime").innerHTML = '当前时间：' + y + "年" + mt.padStart(2,'0') + "月" + day.padStart(2,'0') + "日" + "-" + h.padStart(2,'0') + ":" + m.padStart(2,'0') + ":" + s.padStart(2,'0');
         t = setTimeout(time, 1000); //设定定时器，循环运行
     }
+
     import echarts from 'plugins/echarts'
+
     export default {
         name: "DashBoard",
+        data(){
+          return {
+              dc01_bgp_pd:[100, 520, 200, 334, 390, 330, 220]
+          }
+        },
         mounted() {
             t = setTimeout(time, 1000);//開始运行
             this.draw()
@@ -77,43 +85,81 @@
             draw() {
                 let myChart01 = echarts.init(this.$refs.leftBar)
                 myChart01.setOption({
-                    tooltip:{
-                        show:true,
+                    color: ['#2f89cf'],
+                    tooltip: {
                         trigger: 'axis',
                         axisPointer: {            // 坐标轴指示器，坐标轴触发有效
                             type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
                         }
                     },
-                    legend: {
-                        data: ['裤子', '鞋子'],
-                        top: 15
+                    grid: {
+                        top: '10%',
+                        left: '0%',
+                        right: '0%',
+                        bottom: '4%',
+                        containLabel: true
                     },
-                    // X轴
-                    xAxis: {
-                        data: [
-                            '一月', '二月', '三月', '四月'
-                        ]
-                    },
-                    // Y轴
-                    yAxis: {},
-                    // 数据
+                    xAxis: [
+                        {
+                            type: 'category',
+                            data: ['Sat','Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri' ],
+                            axisTick: {
+                                alignWithLabel: true
+                            },
+                            axisLabel: {
+                                color: "rgba(255,255,255,.6)",
+                                fontSize: "12"
+                            },
+                            axisLine: {
+                                show: false
+                                // 如果想要设置单独的线条样式
+                                // lineStyle: {
+                                //     color: "rgba(255,255,255,.1)",
+                                //     width: 1,
+                                //     type: "solid"
+                                // }
+                            }
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: 'value',
+                            axisLabel: {
+                                textStyle: {
+                                    color: "rgba(255,255,255,.6)",
+                                    fontSize: 12
+                                }
+                            },
+                            axisLine:{
+                                lineStyle:{
+                                    color: "rgba(255,255,255,.1)",
+                                }
+                            },
+                            splitLine: {
+                                lineStyle: {
+                                    color: "rgba(255,255,255,.1)"
+                                }
+                            }
+                        }
+                    ],
                     series: [
                         {
-                            name: '裤子',
+                            name: '峰值带宽',
                             type: 'bar',
-                            data: [120, 100, 440,500]
-                        },
-                        {
-                            name: '鞋子',
-                            type: 'bar',
-                            data: [200, 120, 240, 330]
+                            barWidth: '35%',
+                            data: this.dc01_bgp_pd,
+                            itemStyle: {
+                                // 修改柱子圆角
+                                barBorderRadius: 5
+                            }
                         }
                     ]
-                })
+                });
                 let myChart02 = echarts.init(this.$refs.leftLine)
                 myChart02.setOption({
-                    tooltip:{
-                        show:true
+                    color: ['red', 'yellow', 'green'],
+                    tooltip: {
+                        show: true
                     },
                     legend: {
                         data: ['衣服', '帽子'],
