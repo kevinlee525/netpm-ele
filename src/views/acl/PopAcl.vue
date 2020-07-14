dc822d65d2b96f85a0c08cc39237274a8eacaac9
kevin <kevinlee_168@163.com>
2020-07-14 19:32:52 +0800

    '添加了ACL相关的view及接口'

--- src/views/acl/PopAcl.vue
+++ src/views/acl/PopAcl.vue
@@ -1,19 +1,60 @@
 <template>
     <el-card  class="box-card" shadow="hover">
         <div slot="header" class="header">
-            <el-button type="primary">Pop</el-button>
-
+            <span class="title">
+            <el-button type="warning" >Pop</el-button>
+            </span>
+            <el-input style="width: 185px"
+                      placeholder="请输入业务IP"
+                      v-model="ip"
+                      clearable
+                      @blur="blur()"
+                      @clear="clearData([])">
+            </el-input>
+            <el-button type="primary">提交</el-button>
         </div>
         <div>
-            Go Pop
+            Go Test
         </div>
     </el-card>
 
 </template>
 
 <script>
+    import {getAcl} from "network/acl/acl";
+    import  {isValidIP} from 'common/utils'
+    import {Message} from 'element-ui'
+
     export default {
-        name: "PopAcl"
+        name: "TestAcl",
+        data(){
+          return{
+              ip:''
+          }
+        },
+        methods:{
+            async getacl(){
+                let post_data = {
+                    mgtIP:"10.9.255.92",
+                    GroupID:"3111"
+                };
+                const {data:ret} =  await getAcl({
+                    url:'/acl/',
+                    method:'post',
+                    data:post_data,
+                });
+                console.log(ret);
+
+            },
+            blur() {
+                if (!isValidIP(this.ip)) {
+                    Message({
+                        message: 'Invalid IP!',
+                        type: 'warning'
+                    })
+                }
+            },
+        }
     }
 </script>
 
@@ -27,5 +68,9 @@
     }
     .header {
         text-align: center;
+    }
+    .title {
+        position: absolute;
+        left: 55px;
     }
 </style>
--- src/views/acl/TestAcl.vue
+++ src/views/acl/TestAcl.vue
@@ -1,24 +1,137 @@
 <template>
-    <el-card  class="box-card" shadow="hover">
+    <el-card class="box-card" shadow="hover">
         <div slot="header" class="header">
-            <el-button type="primary" @click="getacl">Test</el-button>
-
+            <span class="title">
+            <el-button type="warning">Test</el-button>
+            </span>
+            <el-input style="width: 185px"
+                      placeholder="请输入业务IP"
+                      v-model="ip"
+                      clearable
+                      @blur="blur()"
+                      @clear="clearData([])">
+            </el-input>
+            <el-button type="primary" @click="getacl">提交</el-button>
         </div>
-        <div>
-            Go Test
-        </div>
+        <el-table
+                :data="tableData"
+                height="500"
+                border
+                :cell-style="rowClass"
+                :header-cell-style="headClass"
+                style="width: 100%">
+            <el-table-column
+                    prop="GroupID"
+                    label="GroupID"
+                    width="130"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="RuleID"
+                    label="RuleID"
+                    width="130"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="Action"
+                    label="Action"
+                    width="130"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="ProtocolType"
+                    label="Protocol"
+                    width="130"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="SrcIPv4"
+                    label="SrcIPv4"
+                    width="130"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="SrcPort"
+                    label="SrcPort"
+                    width="130"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="DstIPv4"
+                    label="DstIPv4"
+                    width="130"
+            >
+            </el-table-column>
+            <el-table-column
+                    prop="DstPort"
+                    label="DstPort"
+                    width="130"
+            >
+            </el-table-column>
+            <el-table-column
+                    fixed="right"
+                    label="操作"
+                    >
+                <template slot-scope="scope">
+                    <el-button
+                            @click.native.prevent="deleteRow(scope.$index, tableData)"
+                            type="text"
+                            size="small">
+                        移除
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </el-card>
 
 </template>
 
 <script>
     import {getAcl} from "network/acl/acl";
+    import {isValidIP} from 'common/utils'
+    import {Message} from 'element-ui'
 
     export default {
         name: "TestAcl",
-        methods:{
-            getacl(){
-                console.log(111);
+        data() {
+            return {
+                ip: '',
+                tableData: []
+            }
+        },
+        methods: {
+            async getacl() {
+                let post_data = {
+                    mgtIP: "10.9.255.92",
+                    GroupID: "3000"
+                    // GroupID: "3111"
+                };
+                const {data: ret} = await getAcl({
+                    url: '/acl/',
+                    method: 'post',
+                    data: post_data,
+                });
+                console.log(ret);
+                this.tableData = ret
+            },
+            // 表头样式设置
+            headClass() {
+                return 'text-align: center;background:#FFEC8B;color:red'
+            },
+            // 表格样式设置
+            rowClass() {
+                return 'text-align: center;'
+            },
+            blur() {
+                if (!isValidIP(this.ip)) {
+                    Message({
+                        message: 'Invalid IP!',
+                        type: 'warning'
+                    })
+                }
+            },
+            deleteRow(index, rows) {
+                rows.splice(index, 1);
             }
         }
     }
@@ -32,7 +145,13 @@
         right: 15px;
         height: 650px;
     }
+
     .header {
         text-align: center;
+    }
+
+    .title {
+        position: absolute;
+        left: 55px;
     }
 </style>
